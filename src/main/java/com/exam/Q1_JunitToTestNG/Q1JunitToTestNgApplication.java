2c74a70cd531344fd3a33ba03d902d09699432ba
Mayur <mayurdesale@gmail.com>
2019-06-15 12:06:24 +0530

    First Commit Without TestNG

---
+++ src/main/java/com/exam/Q1_JunitToTestNG/Q1JunitToTestNgApplication.java
@@ -0,0 +1,13 @@
+package com.exam.Q1_JunitToTestNG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q1JunitToTestNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q1JunitToTestNgApplication.class, args);
+	}
+
+}
